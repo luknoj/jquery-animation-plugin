cba0baa711d0f612f4dc6d18663374116484f05c
mighty-titan <luknoj@gmail.com>
2018-01-16 21:07:36 +0100

    made the animation launch on each group of elements at the same time

--- jquery.plugin.js
+++ jquery.plugin.js
@@ -1,22 +1,20 @@
-(function($){
-  let elements;
+(function ( $ ) {
   $.fn.animate = function(config) {
     var options = $.extend({
       duration: "3000",
       delay: "300",
       type: "fadeIn",
-    },
-  config
-  );  
-  elements = this.children();
-  elements.each((index, element) => {
+    }, config);  
+    for(let i = 0; i < this.length ; i++){
+    $(this[i]).children().each((index, element) => {
       $(element)
-      .css("animation-delay", `${(options.delay/1000)*index}s` )
+      .css("animation-delay", `${(options.delay/1000)*index}s`)
       .css("animation-duration", `${options.duration/1000}s` )
       .addClass(`animated ${options.type}`)
       .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', () => {
-        $(element).removeClass(`animated ${options.type}`);
+        this.removeClass(`animated ${options.type}`);
       });
     })
-  };
-})(jQuery);
+  }
+}
+}(jQuery));

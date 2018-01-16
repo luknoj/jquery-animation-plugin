50fef3dbc2c888850b117e648d4dca955368f019
mighty-titan <luknoj@gmail.com>
2018-01-16 23:12:41 +0100

    lame for to proper map

--- jquery.plugin.js
+++ jquery.plugin.js
@@ -1,20 +1,23 @@
 (function ( $ ) {
   $.fn.animate = function(config) {
-    var options = $.extend({
+    const options = $.extend(
+    {
       duration: "3000",
       delay: "300",
       type: "fadeIn",
-    }, config);  
-    for(let i = 0; i < this.length ; i++){
-    $(this[i]).children().each((index, element) => {
-      $(element)
-      .css("animation-delay", `${(options.delay/1000)*index}s`)
-      .css("animation-duration", `${options.duration/1000}s` )
-      .addClass(`animated ${options.type}`)
-      .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', () => {
-        this.removeClass(`animated ${options.type}`);
+    }, 
+    config
+  );  
+  
+  this.each((index, group) => {
+    $(group)
+      .children()
+      .each((index, element) => {
+        $(element)
+          .css("animation-delay", `${(options.delay / 1000) * index}s`)
+          .css("animation-duration", `${options.duration / 1000}s`)
+          .addClass(`animated ${options.type}`)
       });
-    })
-  }
-}
-}(jQuery));
+    });
+  };
+})(jQuery);
